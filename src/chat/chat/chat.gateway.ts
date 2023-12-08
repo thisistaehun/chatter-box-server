63e8eef18cf21d8a7f76efdb720f09ef8e2dfece
taehunkim <117618060+thisistaehun@users.noreply.github.com>
2023-12-08 16:34:15 +0900

    feat: add stream

--- src/chat/chat/chat.gateway.ts
+++ src/chat/chat/chat.gateway.ts
@@ -1,11 +1,13 @@
 import { Logger } from '@nestjs/common';
 import {
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { interval, take } from 'rxjs';
 import { Server, Socket } from 'socket.io'; // Socket 타입 추가
 import { ChatMessage } from './chat.message.type';
 
@@ -20,20 +22,27 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   server: Server;
 
   handleConnection(client: Socket) {
-    // Socket 타입으로 변경
     Logger.log(`Client connected: ${client.id}`, 'ChatGateway');
   }
 
   handleDisconnect(client: Socket) {
-    // Socket 타입으로 변경
     Logger.log(`Client disconnected: ${client.id}`, 'ChatGateway');
   }
 
-  @SubscribeMessage('chat')
-  handleMessage(client: Socket, message: ChatMessage) {
-    console.log(message);
-    // ChatMessage 타입으로 변경
-    // 클라이언트에서 받은 메시지를 다른 모든 클라이언트에게 전송
-    this.server.emit('chat', message);
-  }
+  @SubscribeMessage('stream')
+  async onEvent(@MessageBody() data: ChatMessage) {
+    // AI Server socket 통신으로 데이터 받아오기
+    const message = `안녕하세요. 저는 온톨이입니다. 여러분에게 도움이 되는 답변을 드리겠습니다. 채팅은 실시간으로 진행되며, 채팅방을 나가면 소켓 연결이 해제됩니다. <EndOfText>`;
+
+    const responseArray = message.split('');
+
+    return interval(400)
+      .pipe(take(responseArray.length))
+      .subscribe((index: number) => {
+        this.server.emit(`stream-${data.sender}`, {
+          sender: 'server',
+          message: responseArray[index],
+          timestamp: new Date(),
+        });
+      });
 }
